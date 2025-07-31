005ff2bd9ca6c4f691906a8bd631f9ceae0e1560
Nikhil Lal Pawa <nikhil.lalpawa@dish.com>
2025-07-31 19:29:26 +0530

    07/31-7:29PM added theme toggle

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,3 +1,13 @@
+import ThemeToggle from "@/components/ThemeToggle";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle className="absolute right-5 top-5" />
+      {/* Bckground effects */}
+      {/* Navbar */}
+      {/* Main Content */}
+      {/* Footer */}
+    </div>
+  );
 };
---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,59 @@
+import { cn, localStorageKeys } from "@/utils/utils";
+import { Moon, Sun } from "lucide-react";
+import React, { useLayoutEffect, useState } from "react";
+
+interface ThemeToggleProps {
+  className?: string;
+}
+
+const ThemeToggle: React.FC<ThemeToggleProps> = ({
+  className,
+}: ThemeToggleProps) => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+  useLayoutEffect(() => {
+    const theme = localStorage.getItem(localStorageKeys.theme);
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    }
+  }, []);
+  return (
+    <button
+      onClick={() =>
+        setIsDarkMode((i: boolean) => {
+          if (!i) {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem(localStorageKeys.theme, "dark");
+          } else {
+            localStorage.setItem(localStorageKeys.theme, "light");
+            document.documentElement.classList.remove("dark");
+          }
+          return !i;
+        })
+      }
+      className={cn(
+        "relative w-[80px] h-[40px] flex items-center rounded-full border-4 border-solid p-2 transition-colors duration-300 ease-in-out",
+        isDarkMode
+          ? "bg-gray-800 border-gray-400"
+          : "bg-yellow-200 border-yellow-500",
+        className
+      )}
+    >
+      <span
+        className={cn(
+          "absolute left-2 top-1/2 -translate-y-1/2 transition-transform duration-300 ease-in-out",
+          "h-[24px] w-[24px] rounded-full flex items-center justify-center",
+          isDarkMode ? "translate-x-[32px]" : "translate-x-0"
+        )}
+      >
+        {isDarkMode ? (
+          <Moon className="text-white" size={20} />
+        ) : (
+          <Sun className="text-yellow-500" size={20} />
+        )}
+      </span>
+    </button>
+  );
+};
+
+export default ThemeToggle;
